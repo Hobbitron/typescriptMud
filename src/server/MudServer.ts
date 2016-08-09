626f0ee33ab812810d1c80663a4311d805068122
hobbitron <edwards8@gmail.com>
2016-08-09 15:50:11 -0700

    Removed Socket.IO dependencies

--- src/server/MudServer.ts
+++ src/server/MudServer.ts
@@ -1,10 +1,16 @@
-let	http = require("http");
-let socketIO	= require("socket.io");
-let fs = require("fs");
+/// <reference path="../../typings/index.d.ts" />
+
+
+import WebSocket = require('ws');
+import http = require('http');
+import fs = require('fs');
+
+var port: number = process.env.PORT || 5000;
+var WebSocketServer = WebSocket.Server;
 
 export class MudServer {
 
-  private handler (req, res) {
+  private handler(req, res) {            
     fs.readFile(__dirname + "/index.html", function (err, data) {
       if (err) {
         res.writeHead(500);
@@ -16,23 +22,16 @@ export class MudServer {
     });
   }
 
-  public start() {
+  public start() {    
+    let serv = http.createServer(this.handler);    
 
-    let serv = http.createServer(this.handler);
+    serv.listen(5001);
 
-    serv.listen(5000);
+    let socketServer = new WebSocketServer({port: port});
 
-    let socketServer = socketIO(serv);
-
-    // socketServer.on('connection', (socket: SocketIO.Socket) => {
-    //         var sc = new socketConnection(socket);
-    //         sc.authenticate().then((success) => {
-    //           this.enterWorld(sc);
-    //         })
-    //         console.log( socket.id + " connected " + new Date())
-    //         if (!this.connectionQueue)
-    //           this.connectionQueue = new Array<socketConnection>();
-    //         this.connectionQueue.push(sc);                        
-    //     });
+    socketServer.on('connection', (sock) => {
+      sock.send("somedata")
+      console.log("connection");
+    });
   }
 }
